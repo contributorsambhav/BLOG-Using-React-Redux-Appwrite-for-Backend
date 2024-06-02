8de66e4028eb41aa1f07b947d30d2853fae7d574
contributorsambhav <sambhav511974@gmail.com>
2024-06-02 22:29:15 +0530

    API key issue troubleshoot

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, PostForm } from '../components'
 
 function AddPost() {
+  useEffect(()=>{
+    <script src="https://cdn.tiny.cloud/1/f5e7bq9wwqrfmhstvnmqadvz8xgrp10zznajfqfj0lor2wvr/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>  
+  })
   return (
     <div className=' bg-gray-200 dark:bg-gray-700 py-8'>
         <Container>
